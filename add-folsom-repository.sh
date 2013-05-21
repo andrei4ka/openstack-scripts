48e3938764534acbbd26c5a31ed8e93973cd1982
Kirilochkin Andrey <akirilochkin@mirantis.com>
2013-05-21 13:48:43 +0400

    Removing old scripts

--- add-folsom-repository.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-. $(dirname $(readlink -f $0))/00-lib.sh
-
-check_root
-
-echo deb http://ubuntu-cloud.archive.canonical.com/ubuntu precise-updates/folsom main >> /etc/apt/sources.list.d/folsom.list &&	apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 5EDB1B62EC4926EA
-
-apt-get update
